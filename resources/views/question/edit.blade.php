7dbb8745d20986626b1fdd51b1739456b20c68e8
yuli setyawan <yulisetyawan2008@gmail.com>
2020-07-11 11:34:35 +0800

    tes

--- resources/views/question/edit.blade.php
+++ resources/views/question/edit.blade.php
@@ -18,7 +18,8 @@
                   </div>
                   <div class="form-group">
                     <label for="isi">Masukkan Pertanyaan</label>
-                    <input type="text" class="form-control" id="isi" name = "isi" value="{!!$question->isi!!}" placeholder="Isi Pertanyaan">
+                    <textarea class="form-control" id="isi" name = "isi"  cols="30" rows="10" value="{{$question->isi}}"></textarea>
+                    <input type="text" class="form-control" id="isi" name = "isi" value="{{$question->isi}}">
                   </div>
                   <input hidden type="text" name="created_at" value="{{$question->created_at}}">
                   <input hidden type="text" name="updated_at" value="{{ \Carbon\Carbon::now() }}">
@@ -30,5 +31,20 @@
               </form>
         </div>
     </div>
+
+    // value belum bisa masuk otomatis -->
+
+    <script src="https://cdn.ckeditor.com/ckeditor5/20.0.0/classic/ckeditor.js"></script>
+   
+    <script>
+      ClassicEditor
+                                .create( document.querySelector( '#isi' ) )
+                                .then( isi => {
+                                        console.log( isi );
+                                } )
+                                .catch( error => {
+                                        console.error( error );
+                                } );
+    </script>
 
 @endsection
